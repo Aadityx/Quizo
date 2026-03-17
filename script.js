a9465e26f6f9e3ea2483b8596843e4dc24025506
Aadityx <aadityapatil784@gmail.com>
2026-03-17 19:42:40 +0530

    js updated

--- script.js
+++ script.js
@@ -4,35 +4,33 @@ let selected = -1;
 const options = document.querySelectorAll(".options button");
 const submit = document.getElementById("submit");
 
-// select option
+//option selected
 options.forEach((btn, index) => {
   btn.onclick = () => {
-    selected = index;
+    selected = index; //index of selected option
 
-    // reset all colors
-    options.forEach(b => b.style.background = "#f5f5f5");
+    //option change
+   options.forEach(opt => opt.style.background = "#f5f5f5");
 
     // highlight selected
     btn.style.background = "yellow";
   };
 });
 
-// submit answer
-submit.onclick = () => {
-  if (selected === -1) {
-    alert("Select an option first!");
-    return;
-  }
 
-  // correct answer = 2 (Delhi)
+submit.onclick = () => {
+
+    // correct answer = 2 (Delhi)
   if (selected === 2) {
     options[selected].style.background = "green";
     score++;
-  } else {
+  } 
+
+  else {
     options[selected].style.background = "red";
     options[2].style.background = "green";
   }
 
   // update score
-  document.querySelector(".score").innerText = "Score: " + score;
+  document.querySelector(".score").innerText = "Score: " + 1;
 };
